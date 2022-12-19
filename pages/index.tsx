183e15009dc77443d609d35d74dd3b7960b94062
gorvgoyl <1gouravgg@gmail.com>
2022-12-20 01:37:44 +0700

    favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
 import { TitleUI } from "../components/TitleUI";
 import { ChromeLogo } from "../components/icons";
 import { formStyles } from "../styles/defaultStyles";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
