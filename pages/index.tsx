29c9d55019acfb442d2ae1a2019b61fb96628e38
Gourav Goyal <1gouravgg@gmail.com>
2023-01-14 10:56:54 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,7 +48,7 @@ export default function Home() {
                 <Star size="22" />
               </div>
               <div>
-                <b>10,000+</b> happy users who don't like writing emails
+                <b>20,000+</b> happy users who don't like writing emails
               </div>
               {/* <ChromeStars
                 id="pdnenlnelpdomajfejgapbdpmjkfpjkp"
